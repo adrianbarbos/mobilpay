9ac360679d4940475f760b499c5e8a073a503a78
adrianbarbos <ady.barbos@gmail.com>
2017-04-06 12:20:21 +0300

    Bug fix

--- src/MobilpayGateway.php
+++ src/MobilpayGateway.php
@@ -7,29 +7,28 @@ use Omnipay\Omnipay;
 class MobilpayGateway {
 
     protected $data;
-    protected $gateway;
 
     use DataTrait;
 
     function __construct() {
-
-        $this->gateway = Omnipay::create('MobilPay')->setMerchantId(config('mobilpay.merchant_id'));
-
         $this->initData();
     }
 
     public function purchase() {
-        $this->gateway->setPublicKey(config('mobilpay.public_key_path'));
+        $gateway = Omnipay::create('MobilPay');
+        $gateway->setMerchantId(config('mobilpay.merchant_id'));
+        $gateway->setPublicKey(config('mobilpay.public_key_path'));
 
-        $response = $this->gateway->purchase($this->data)->send();
+        $response = $gateway->purchase($this->data)->send();
 
         $response->redirect();
     }
 
     public function response() {
-        $this->gateway->privateKeyPath(config('mobilpay.private_key_path'));
+        $gateway = Omnipay::create('MobilPay');
+        $gateway->privateKeyPath(config('mobilpay.private_key_path'));
 
-        $response = $this->gateway->completePurchase($_POST)->send();
+        $response = $gateway->completePurchase($_POST)->send();
         $response->sendResponse();
 
         return $response;
